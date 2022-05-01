35ae253bc22678c636628f9ca5f05b0f7a5762ac
Laurence Qi <laurence.qi@outlook.com>
2022-05-01 11:43:38 +1000

    removed the console.logs

--- api/helpers/ranking.js
+++ api/helpers/ranking.js
@@ -4,20 +4,15 @@ const Word = require("../models/word");
 const rank = async(textSnippets) => {
     const result = await Promise.all(
         textSnippets.map(async text => {
-            let words = text.split(' ')
+            const words = text.split(' ')
 
-            console.log(words);
             const complexityObjs = await Word.find({
                 word: {
                     $in: words
                 }
             });
 
-            console.log(complexityObjs);
-            // TODO: with this array reduce down to a single value. 
-
             const complexity = complexityObjs.reduce((prev, curr) => prev + curr.rank, 0);
-            console.log(complexity);
             return { text, complexity };
         })
     )
