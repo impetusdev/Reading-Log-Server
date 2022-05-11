4fba9ee2f630c6e400b889fcf0e50d3c1b90dac5
Laurence Qi <laurence.qi@outlook.com>
2022-05-12 09:17:54 +1000

    added .env

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,13 +2,32 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler')
 const User = require('../models/user');
+require('dotenv').config()
+
 // @desc Register
 // @route POST /register
 // @access Public
 // TODO: extract all of the validation logic into helper files.
 
 const loginUser = asyncHandler(async(req, res) => {
-    res.json({ message: 'Logib User' });
+    // get the req
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    console.log(`User password is: ${user.password}`);
+    if (user && (await bcrypt.compare(password, user.password))) {
+
+        res.json({
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user.id)
+        });
+    } else {
+        res.status(400)
+        throw new Error('Username or password is incorrect');
+    }
 })
 
 const registerUser = asyncHandler(async(req, res) => {
@@ -39,18 +58,24 @@ const registerUser = asyncHandler(async(req, res) => {
         res.status(201).json({
             _id: user.id,
             username,
-            email
+            email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
         throw new Error('Invalid user data')
     }
-
 })
 
 const getMe = asyncHandler(async(req, res) => {
     res.json({ message: 'Get Current User' });
 })
+
+// generate JWT
+const generateToken = (id) => {
+    console.log('hello')
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+}
 
 module.exports = {
     loginUser,
